e44cabea4003449b73160e92c64f55830425bc34
DropCoffee <malyukov.maksim90@yandex.ru>
2024-06-19 20:44:21 +0300

    2. Installing Spark

--- src/main/java/org/sandbox/App.java
+++ src/main/java/org/sandbox/App.java
@@ -1,10 +1,30 @@
 package org.sandbox;
 
-/**
- * Hello world!
- */
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Logger.getLogger("org.apache").setLevel(Level.WARNING);
+
+        List<Double> inputData = new ArrayList<>();
+        inputData.add(100.5);
+        inputData.add(100.5);
+        inputData.add(24.3);
+        inputData.add(26.7);
+        System.out.println(inputData);
+
+        SparkConf conf = new SparkConf().setAppName("first app").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<Double> myRdd = sc.parallelize(inputData);
+
+        sc.close();
     }
 }
