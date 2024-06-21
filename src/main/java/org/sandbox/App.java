81383283056e9409c4afc9aa76c33bf531b9f549
DropCoffee <malyukov.maksim90@yandex.ru>
2024-06-21 20:54:22 +0300

    4.1. Mapping Operations

--- src/main/java/org/sandbox/App.java
+++ src/main/java/org/sandbox/App.java
@@ -13,21 +13,23 @@ public class App {
     public static void main(String[] args) {
         Logger.getLogger("org.apache").setLevel(Level.WARNING);
 
-        List<Double> inputData = new ArrayList<>();
-        inputData.add(100.5);
-        inputData.add(100.5);
-        inputData.add(24.3);
-        inputData.add(26.7);
+        List<Integer> inputData = new ArrayList<>();
+        inputData.add(100);
+        inputData.add(100);
+        inputData.add(24);
+        inputData.add(26);
         System.out.println(inputData);
 
         SparkConf conf = new SparkConf().setAppName("first app").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<Double> myRdd = sc.parallelize(inputData);
+        JavaRDD<Integer> myRdd = sc.parallelize(inputData);
 
-        double result = myRdd.reduce((x1, x2) -> x1 + x2);
+        int result = myRdd.reduce((x1, x2) -> x1 + x2);
 
         System.out.println(result);
+
+        JavaRDD<Double> sqrtRdd = myRdd.map(x1 -> Math.sqrt(x1));
 
         sc.close();
     }
